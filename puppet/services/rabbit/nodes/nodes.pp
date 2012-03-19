135bd229ac498508a347e1574622527c15fe01e3
Jussi Heinonen <heinonen.jussi@gmail.com>
2012-03-19 13:22:09 +0000

    nodes.pp tidy-up

--- puppet/services/rabbit/nodes/nodes.pp
+++ puppet/services/rabbit/nodes/nodes.pp
@@ -1,11 +1,7 @@
-
-
-
 node /^rabbitmq-server.*/ {
 	require initialise
 	class { 'rabbitmq': version => '2.7.1', enabled => true }
-	#-> 
-	#class { 'rabbitmq::service': enabled => true }
+
 }
 
 node /^haproxy.*/ {
